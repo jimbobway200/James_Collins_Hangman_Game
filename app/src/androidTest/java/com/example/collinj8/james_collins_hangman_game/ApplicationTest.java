5efc0983718bbb5f8434494e5c2bdec5488b3bf8
James Collins <jimbobway200@hotmail.com>
2015-04-13 12:44:04 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/collinj8/james_collins_hangman_game/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.collinj8.james_collins_hangman_game;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
