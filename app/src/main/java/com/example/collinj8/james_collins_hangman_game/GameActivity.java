2507bc9518ddc806d4f1a145074ef8162172a50f
James Collins <jimbobway200@hotmail.com>
2015-04-15 21:06:24 -0700

    got most logic working...working on getting the picture to change...

--- app/src/main/java/com/example/collinj8/james_collins_hangman_game/GameActivity.java
+++ app/src/main/java/com/example/collinj8/james_collins_hangman_game/GameActivity.java
@@ -1,43 +1,128 @@
 package com.example.collinj8.james_collins_hangman_game;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class GameActivity extends ActionBarActivity {
 
     //Variables
     public static int current_word_itt = 0;
+    public static int wrong_answer_count = 0;
+    private String[] picture_array = {"hangman0.png", "hangman1.png", "hangman2.png", "hangman3.png" , "hangman4.png", "hangman5.png", "hangman6.png"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //Variables
-    //    public static int current_word_itt = 0;
+        // Grab fields and views from user view (activity_main.xml)
+        final EditText user_guess = (EditText) findViewById(R.id.user_guess);
+        final TextView current_guessed_letters = (TextView) findViewById(R.id.guessed_letters);
+        final TextView revealed_word = (TextView) findViewById(R.id.guess_word);
+        final Button guess_button = (Button) findViewById(R.id.button_guess);
+        final TextView num_remaining_guesses = (TextView) findViewById(R.id.num_guesses_left);
 
-        // Grab fields from user view (activity_main.xml)
-        EditText user_guess= (EditText) findViewById(R.id.user_guess);
-        TextView current_guessed_letters = (TextView) findViewById(R.id.current_guesses);
-        TextView remaining_guesses = (TextView) findViewById(R.id.remaining_guesses);
-        TextView revealed_word= (TextView) findViewById(R.id.guess_word);
-
-        String game_word = setupgame(current_word_itt);
+        //variables
+        final String game_word = setupgame(current_word_itt);
         int game_word_length = game_word.length();
 
         //Add Watcher for button "GUESS"
-        Button guess_button = (Button) findViewById(R.id.button_guess);
         guess_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
 
+                    String active_guess = user_guess.getText().toString();
+                    String current_guessed_string = current_guessed_letters.getText().toString();
+                    String revealed_word_string = revealed_word.getText().toString();
+                    ImageView img = (ImageView) findViewById(R.id.current_picture_status);
+
+                    //Has the user guess this already?
+                    boolean already_guessed = current_guessed_string.contains(active_guess);
+                    if (already_guessed) {
+                        //display in long period of time
+                        Toast toast = Toast.makeText(getApplicationContext(), "You have already guessed this letter",
+                                Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+                        toast.show();
+                    }
+                    else
+                    {
+                        //Has not guessed this letter yet
+                        int char_found_loc = 0;
+                        boolean found = false;
+                        StringBuilder temp = new StringBuilder(revealed_word_string);
+                        while (char_found_loc != -1) {
+                            char_found_loc = game_word.indexOf(active_guess, char_found_loc + 1);
+                            if (char_found_loc != -1) { //Did we find his guess in our game word?
+                                temp.setCharAt(char_found_loc, active_guess.charAt(0));
+                                found = true;
+                            }
+                        }
+                        //So did we ever find a match?
+                        if (found) {
+                            //Update Current guessed letters
+                            StringBuilder new_string_current_guesses = new StringBuilder(current_guessed_string);
+                            new_string_current_guesses.append(active_guess);
+                            current_guessed_letters.setText(new_string_current_guesses);
+
+                            //Update revealed word
+                            revealed_word.setText(temp);
+
+                        } else { //wrong guess
+                            wrong_answer_count++;
+                            //updated picture and remaining guesses
+                            if (wrong_answer_count == 1) {
+                                img.setImageResource(R.mipmap.hangman1);
+                                num_remaining_guesses.setText("5");
+                            } else if (wrong_answer_count == 2) {
+                                img.setImageResource(R.mipmap.hangman2);
+                                num_remaining_guesses.setText("4");
+                            } else if (wrong_answer_count == 3) {
+                                img.setImageResource(R.mipmap.hangman3);
+                                num_remaining_guesses.setText("3");
+                            } else if (wrong_answer_count == 4) {
+                                img.setImageResource(R.mipmap.hangman4);
+                                num_remaining_guesses.setText("2");
+                            } else if (wrong_answer_count == 5) {
+                                img.setImageResource(R.mipmap.hangman5);
+                                num_remaining_guesses.setText("1");
+                            } else if (wrong_answer_count == 6) { //GAME OVER
+                                //Hide the Keyboard
+                                InputMethodManager inputManager = (InputMethodManager)
+                                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                                        InputMethodManager.HIDE_NOT_ALWAYS);
+
+                                //Update Picture and other aspects on page
+                                img.setImageResource(R.mipmap.hangman6);
+                                guess_button.setVisibility(View.GONE); //Hide Guess Button
+                                num_remaining_guesses.setText("0");
+                                Toast.makeText(getApplicationContext(), "Sorry! You Lost! Try Again...",
+                                        Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+
+                    }
+                }//END TRY BLOCK
+                catch (Exception e) { //User Didn't enter anything
+                    Toast.makeText(getApplicationContext(), "Please enter a guess!",
+                            Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -80,11 +165,13 @@ public class GameActivity extends ActionBarActivity {
 
     public String setupgame (int i)
     {
-        // Grab fields from user view (activity_main.xml)
+        // Grab fields and views from user view (activity_main.xml)
         EditText user_guess= (EditText) findViewById(R.id.user_guess);
         TextView current_guessed_letters = (TextView) findViewById(R.id.guessed_letters);
         TextView num_remaining_guesses = (TextView) findViewById(R.id.num_guesses_left);
         TextView revealed_word= (TextView) findViewById(R.id.guess_word);
+        Button guess_button = (Button) findViewById(R.id.button_guess);
+        ImageView img = (ImageView) findViewById(R.id.current_picture_status);
 
         //Get array I created from strings.xml
         String[] word_array = getResources().getStringArray(R.array.words);
@@ -105,6 +192,15 @@ public class GameActivity extends ActionBarActivity {
 
         //Set Number of guess left to default
         num_remaining_guesses.setText("6");
+
+        //Show Guess Button
+        guess_button.setVisibility(View.VISIBLE);
+
+        //Set Default Picture for hangman
+        img.setImageResource(R.mipmap.hangman0);
+
+        //Reset Wrong Answer Count
+        wrong_answer_count = 0;
 
         return new_word;
     }
